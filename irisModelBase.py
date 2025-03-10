610889288067b19d5b25b06a327ac3e8f68e7425
108-0 <jk1028@g.shingu.ac.kr>
2025-03-10 16:01:34 +0900

    KNeighborsClassifier를 사용한 붓꽃분류

--- irisModelBase.py
+++ irisModelBase.py
@@ -1,9 +1,11 @@
 #2025.3.10.
 #프로젝트 붓꽃분류
 #이용희교수님과 열심히 해보자
+import joblib
 import pandas as pd
 import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
 
 iris_df = pd.read_csv('iris.csv')
 print(iris_df)
@@ -12,17 +14,42 @@ print(iris_df['sepal_length'])
 y= iris_df['species']
 print(y)
 
-x= iris_df.drop('species', axis=1)
-print(x)
+X= iris_df.drop('species', axis=1)
+print(X)
 
 kn = KNeighborsClassifier()
-model_kn = kn.fit(x, y)
+rfc = RandomForestClassifier()
+
+model_kn = kn.fit(X, y)
+model_rfc = rfc.fit(X, y)
+
+joblib.dump(model_rfc, 'model_rfc.pkl')
 
 # x_new = np.array([[3,3,3,3]])
-#
-x_new = np.array([[5.0,3.4,1.4,0.2]])
-# setosa [[1. 0. 0.]]
+# ['versicolor']
+# [[0.  0.8 0.2]]
+# ['virginica']
+# [[0.03 0.42 0.55]]
+
+# x_new = np.array([[5.0,3.4,1.4,0.2]])
+# ['setosa']
+# [[1. 0. 0.]]
+# ['setosa']
+# [[1. 0. 0.]]
+
+x_new = np.array([[1,4.2,1.4,7]])
+# ['versicolor']
+# [[0.2 0.6 0.2]]
+# ['setosa']
+# [[0.45 0.23 0.32]]
+
+model_rfc = joblib.load('model_rfc.pkl')
+
 pred = model_kn.predict(x_new)
 print(pred)
 probavility = model_kn.predict_proba(x_new)
+print(probavility)
+pred = model_rfc.predict(x_new)
+print(pred)
+probavility = model_rfc.predict_proba(x_new)
 print(probavility)
---
+++ main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+import uvicorn
+
+from irisModel import IrisMuchineLearning, IrisSpecies
+
+app = FastAPI()
+
+model = IrisMuchineLearning()
+
+@app.get("/")
+async def root():
+    return {"message":"Hello this is iris classfier 25.3.10"}
+
+@app.get("/predict")
+async def predict():
+    pred = model.predict_species(5.0,3.4,1.4,0.2)
+    # pred = "아메리카뒷부리장다리물떼새"
+    return {"prediction": pred}
+
+@app.post("/predict")
+async def pridict_species(iris:IrisSpecies):
+    pred = model.predict_species((iris.sepal_length, iris.sepal_width, iris.petal_length, iris.petal_width))
+    return pred
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8000)
